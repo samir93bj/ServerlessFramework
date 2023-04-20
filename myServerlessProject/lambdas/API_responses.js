12524a9ae5d8d9d844def27ea6aedeae577e011d
samir mahmud <samir93bj@gmail.com>
2023-04-20 19:01:21 -0300

    feat: add response status 200

---
+++ myServerlessProject/lambdas/API_responses.js
@@ -0,0 +1,13 @@
+const Responses = {
+	_200(data = {}){
+		return {
+			Headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Methods': '*',
+				'Access-Control-Allow-Origin': '*'
+			},
+			stausCode: 200,
+			body: JSON.stringify(data)
+		}
+	}
+}
