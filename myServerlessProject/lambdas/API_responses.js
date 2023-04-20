faf2922ce9fd806aa1c97466c25d08ae959359ed
samir mahmud <samir93bj@gmail.com>
2023-04-20 19:02:04 -0300

    feat: add response status 400

--- myServerlessProject/lambdas/API_responses.js
+++ myServerlessProject/lambdas/API_responses.js
@@ -9,5 +9,18 @@ const Responses = {
 			stausCode: 200,
 			body: JSON.stringify(data)
 		}
+	},
+	_400(data = {}){
+		return {
+			Headers: {
+				'Content-Type': 'application/json',
+				'Access-Control-Allow-Methods': '*',
+				'Access-Control-Allow-Origin': '*'
+			},
+			stausCode: 400,
+			body: JSON.stringify(data)
+		}
 	}
 }
+
+module.exports = Responses
