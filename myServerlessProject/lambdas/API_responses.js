067dcc58693e4a94648da0057628eba5ae636e04
samir mahmud <samir93bj@gmail.com>
2023-04-21 14:17:36 -0300

    fix :solved fix in reponses

--- myServerlessProject/lambdas/API_responses.js
+++ myServerlessProject/lambdas/API_responses.js
@@ -1,26 +1,29 @@
 const Responses = {
-	_200(data = {}){
-		return {
-			Headers: {
-				'Content-Type': 'application/json',
-				'Access-Control-Allow-Methods': '*',
-				'Access-Control-Allow-Origin': '*'
-			},
-			stausCode: 200,
-			body: JSON.stringify(data)
-		}
-	},
-	_400(data = {}){
-		return {
-			Headers: {
-				'Content-Type': 'application/json',
-				'Access-Control-Allow-Methods': '*',
-				'Access-Control-Allow-Origin': '*'
-			},
-			stausCode: 400,
-			body: JSON.stringify(data)
-		}
-	}
-}
+    _200(data = {}) {
+			return {
+				headers: {
+						'Content-Type': 'application/json',
+						'Access-Control-Allow-Methods': '*',
+						'Access-Control-Allow-Origin': '*',
+				},
+				statusCode: 200,
+				body: JSON.stringify(data),
+			};
+    },
+
+    _400(data = {}) {
+			return {
+				headers: {
+						'Content-Type': 'application/json',
+						'Access-Control-Allow-Methods': '*',
+						'Access-Control-Allow-Origin': '*',
+				},
+				statusCode: 400,
+				body: JSON.stringify(data),
+			};
+    },
+};
+
+module.exports = Responses;
 
 module.exports = Responses
