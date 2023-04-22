126eb11b5e608bd6bcae2bb747ea9854665aa3c4
samir mahmud <samir93bj@gmail.com>
2023-04-22 13:38:52 -0300

    feat: add service get file

--- myServerlessProject/lambdas/common/S3.js
+++ myServerlessProject/lambdas/common/S3.js
@@ -3,7 +3,24 @@ const AWS = require('aws-sdk');
 const s3Client = new AWS.S3()
 
 const S3 = {
-	async get () {},
+	async get (fileName, bucket) {
+		const params = {
+			Bucket: bucket,
+			Key: fileName
+		}
+
+		let data = await s3Client.getObject(params).promise()
+
+		if (!data) {
+			throw new Error(`Failed to get file ${fileName} from ${bucket}`)
+		}
+
+		if (fileName.slice(fileName.length - 4, fileName.length) === 'json') {
+			data = data.Body.toString()
+		}
+
+		return data
+	},
 	async write (data, fileName,bucket) {
 		const params = {
 			Bucket: bucket,
