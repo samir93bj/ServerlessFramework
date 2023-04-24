c4815fe7f2aaf2c496f10b7bfed3aa662f028e90
samir mahmud <samir93bj@gmail.com>
2023-04-24 13:39:27 -0300

    feat: add config message

---
+++ myServerlessProject/lambdas/websockets/message.js
@@ -0,0 +1,7 @@
+const Responses = require('../common/API_responses');
+
+exports.handler = async (event) => {
+	console.log('event: ' + event);
+
+	return Responses._200({ message: 'got a message' });
+};
