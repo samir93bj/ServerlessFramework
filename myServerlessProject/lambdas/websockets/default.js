7bbbe6f88ab1bb4a12ef9dc8b6ded6022d0a2a6a
samir mahmud <samir93bj@gmail.com>
2023-04-24 13:39:00 -0300

    feat: add config disconnect

--- myServerlessProject/lambdas/websockets/default.js
+++ myServerlessProject/lambdas/websockets/default.js
@@ -3,5 +3,5 @@ const Responses = require('../common/API_responses');
 exports.handler = async (event) => {
 	console.log('event: ' + event);
 
-	return Responses._200({ message: 'connected'});
+	return Responses._200({ message: 'default'});
 };
