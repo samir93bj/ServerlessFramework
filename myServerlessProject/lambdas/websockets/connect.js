fc5364a3c8d4553ec70a1f34a0a8f367462ecfe0
samir mahmud <samir93bj@gmail.com>
2023-04-24 13:58:34 -0300

    feat: add config connection

--- myServerlessProject/lambdas/websockets/connect.js
+++ myServerlessProject/lambdas/websockets/connect.js
@@ -1,7 +1,25 @@
 const Responses = require('../common/API_responses');
+const Dynamo = require('../common/Dynamo');
+
+const tableName = process.env.tableName;
 
 exports.handler = async (event) => {
-	console.log('event: ' + event);
+	try {
+		console.log('event: ' + event);
 
-	return Responses._200({ message: 'connected'});
+		const { connectionId: connectionID } = event.requestContext;
+	
+		const data = {
+			ID: connectionID,
+			date: Date.now(),
+			messages: []
+		}
+	
+		await Dynamo.write(data, tableName);
+	
+		return Responses._200({ message: 'connected'});
+	} catch (err) { 
+		return Responses._500({ message: err.message });
+	}
+	
 };
