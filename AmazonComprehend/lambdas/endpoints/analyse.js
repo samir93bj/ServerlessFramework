87b06e2cc8cd03adf1273ccda7be162d4e58e1a6
samir mahmud <samir93bj@gmail.com>
2023-04-24 21:55:13 -0300

    fix: solved fix in lambda function

--- AmazonComprehend/lambdas/endpoints/analyse.js
+++ AmazonComprehend/lambdas/endpoints/analyse.js
@@ -1,15 +1,33 @@
 const Response = require('../common/API_Responses');
+const AWS = require('aws-sdk');
 
-exports.handler = event => {
+const Comprehend = new AWS.Comprehend();
+
+exports.handler = async event => {
 	try {
 		const body = JSON.parse(event.body);
 
-		if (!body || !body.text) {
-			return Response._400({ message: 'Text read ok.'});
-		}
+    if (!body || !body.text) {
+        return Response._400({ message: 'no text field on the body' });
+    }
 
-		return Response._200({ message: 'Text read ok.' });
+    const text = body.text;
+
+    const params = {
+        LanguageCode: 'en',
+        TextList: [text],
+    };
+
+		const entityResults = await Comprehend.batchDetectEntities(params).promise();
+		const entities = entityResults.ResultList[0];
+
+		const sentimentResults = await Comprehend.batchDetectSentiment(params).promise();
+		const sentiment = sentimentResults.ResultList[0];
+
+		const responseData = [entities, sentiment];
+
+		return Response._200({ message: responseData });
 	} catch (error) {
-		return Response._500({ message: 'Error event received.'});	
+		return Response._500({ message: error.message});	
 	}
 }
