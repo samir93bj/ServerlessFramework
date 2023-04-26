daed98f17fa1d10dcdb09f259e316a28a8cd0cb2
samir mahmud <samir93bj@gmail.com>
2023-04-26 10:27:02 -0300

    add middleware in get player score

--- myServerlessProject/lambdas/endpoints/getPlayerScore.js
+++ myServerlessProject/lambdas/endpoints/getPlayerScore.js
@@ -5,21 +5,26 @@ const { withHooks } = require('../common/hooks');
 const tableName = process.env.tableName;
 
 handler = async event => {
-	if (!event.pathParameters.ID) {
-			return Responses._400({ message: 'missing the ID from the path' });
+		try {
+			if (!event.pathParameters.ID) {
+				return Responses._400({ message: 'missing the ID from the path' });
+		}
+
+		let ID = event.pathParameters.ID;
+
+		const user = await Dynamo.get(ID, tableName).catch (err  => {
+			return null
+		})
+
+		if (!user) {
+			return Responses._400({ message: 'Failed to get user By ID'})
+		}
+
+		return Responses._200({ user })
+	} catch (error) {
+		return Responses._500({ message: `Error: , ${error.message}`});
 	}
-
-	let ID = event.pathParameters.ID;
-
-	const user = await Dynamo.get(ID, tableName).catch (err  => {
-		return null
-	})
-
-	if (!user) {
-		return Responses._400({ message: 'Failed to get user By ID'})
-	}
-
-	return Responses._200({ user })
+	
 };
 
 exports.handler =  withHooks(handler);
