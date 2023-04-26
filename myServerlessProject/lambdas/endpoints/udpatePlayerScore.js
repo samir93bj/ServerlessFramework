9dbd9ae3044bad08c636d9657f9069051af2957f
samir mahmud <samir93bj@gmail.com>
2023-04-26 13:55:39 -0300

    feat: add and initilize updatePlayerScore

---
+++ myServerlessProject/lambdas/endpoints/udpatePlayerScore.js
@@ -0,0 +1,12 @@
+const Responses = require('../common/API_responses');
+const Dynamo = require('../common/Dynamo');
+const { hooksWithValidation } = require('../common/hooks');
+const yup = require('yup')
+
+const handler = async event => {
+	try {
+		
+	} catch (error) {
+		
+	}
+};
