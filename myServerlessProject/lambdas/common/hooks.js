39bc39bc5540c7d9c9feb4c206aa03e2892611ea
samir mahmud <samir93bj@gmail.com>
2023-04-25 22:53:02 -0300

    feaT: add function validate event path

--- myServerlessProject/lambdas/common/hooks.js
+++ myServerlessProject/lambdas/common/hooks.js
@@ -1,14 +1,14 @@
 const { useHooks, logEvent, parseEvent, handleUnexpectedError } = require('lambda-hooks');
 
 const withHooks = useHooks({
-	before: [logEvent, parseEvent, validateEventBody],
+	before: [logEvent, parseEvent, validateEventBody, validatePaths],
 	after:[],
 	onError: [handleUnexpectedError]
 });
 
 const hooksWithValidation = ({ bodySchema, pathSchema }) => {
 	return useHooks({
-		before: [logEvent, parseEvent, ],
+		before: [logEvent, parseEvent ],
 		after:[],
 		onError: [handleUnexpectedError]
 	}, {
@@ -26,9 +26,26 @@ const validateEventBody = async state => {
 		const { event } = state;
 
 		await bodySchema.validate(event.body, { strict: true });
-
 	} catch (error) {
 		console.log('Yup validation error of event.body ', error.message);
+		state.exit = true
+		state.response = { statusCode: 400, body: JSON.stringify({ error: error.message }) }
+	}
+
+	return state;
+}
+
+const validatePaths = async state => {
+	try {
+		const { pathSchema } = state.config;
+
+		if (!pathSchema) throw Error('Missing the required path.');
+
+		const { event } = state;
+
+		await pathSchema.validate(event.pathParamethers, { strict: true });
+	} catch (error) {
+		console.log('Yup validation error of path parameters ', error.message);
 		state.exit = true
 		state.response = { statusCode: 400, body: JSON.stringify({ error: error.message }) }
 	}
