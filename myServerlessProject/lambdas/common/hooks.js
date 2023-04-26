f278d242565f138e0db8e7a98702c58411903815
samir mahmud <samir93bj@gmail.com>
2023-04-25 22:50:03 -0300

    feaT: add function validate event body

--- myServerlessProject/lambdas/common/hooks.js
+++ myServerlessProject/lambdas/common/hooks.js
@@ -1,20 +1,39 @@
 const { useHooks, logEvent, parseEvent, handleUnexpectedError } = require('lambda-hooks');
 
 const withHooks = useHooks({
-	before: [logEvent, parseEvent],
+	before: [logEvent, parseEvent, validateEventBody],
 	after:[],
 	onError: [handleUnexpectedError]
 });
 
 const hooksWithValidation = ({ bodySchema, pathSchema }) => {
 	return useHooks({
-		before: [logEvent, parseEvent],
+		before: [logEvent, parseEvent, ],
 		after:[],
 		onError: [handleUnexpectedError]
 	}, {
 		bodySchema,
 		pathSchema
 	})
+}
+
+const validateEventBody = async state => {
+	try {
+		const { bodySchema } = state.config;
+
+		if (!bodySchema) throw Error('Missing the required body.');
+
+		const { event } = state;
+
+		await bodySchema.validate(event.body, { strict: true });
+
+	} catch (error) {
+		console.log('Yup validation error of event.body ', error.message);
+		state.exit = true
+		state.response = { statusCode: 400, body: JSON.stringify({ error: error.message }) }
+	}
+
+	return state;
 }
 
 module.exports = {
