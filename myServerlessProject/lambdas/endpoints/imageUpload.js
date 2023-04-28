90d55de1abf8ca5935855105ad561fe8306c98ad
samir mahmud <samir93bj@gmail.com>
2023-04-28 12:08:13 -0300

    refactor: imageupload: refacto spaces

--- myServerlessProject/lambdas/endpoints/imageUpload.js
+++ myServerlessProject/lambdas/endpoints/imageUpload.js
@@ -8,53 +8,53 @@ const s3 = new AWS.S3();
 const allowedMimes = ['image/jpeg', 'image/png', 'image/jpg'];
 
 exports.handler = async event => {
-    try {
-        const body = JSON.parse(event.body);
+	try {
+		const body = JSON.parse(event.body);
 
-        if (!body || !body.image || !body.mime) {
-            return Response._400({ message: 'incorrect body on request' });
-        }
+		if (!body || !body.image || !body.mime) {
+				return Response._400({ message: 'incorrect body on request' });
+		}
 
-        if (!allowedMimes.includes(body.mime)) {
-            return Response._400({ message: 'mime is not allowed ' });
-        }
+		if (!allowedMimes.includes(body.mime)) {
+				return Response._400({ message: 'mime is not allowed ' });
+		}
 
-        let imageData = body.image;
-        if (body.image.substr(0, 7) === 'base64,') {
-            imageData = body.image.substr(7, body.image.length);
-        }
+		let imageData = body.image;
+		if (body.image.substr(0, 7) === 'base64,') {
+				imageData = body.image.substr(7, body.image.length);
+		}
 
-        const buffer = Buffer.from(imageData, 'base64');
-        const fileInfo = await fileType.fileTypeFromBuffer(buffer);
-        const detectedExt = fileInfo.ext;
-        const detectedMime = fileInfo.mime;
+		const buffer = Buffer.from(imageData, 'base64');
+		const fileInfo = await fileType.fileTypeFromBuffer(buffer);
+		const detectedExt = fileInfo.ext;
+		const detectedMime = fileInfo.mime;
 
-        if (detectedMime !== body.mime) {
-            return Response._400({ message: 'mime types dont match' });
-        }
+		if (detectedMime !== body.mime) {
+				return Response._400({ message: 'mime types dont match' });
+		}
 
-        const name = uuid();
-        const key = `${name}.${detectedExt}`;
+		const name = uuid();
+		const key = `${name}.${detectedExt}`;
 
-        console.log(`writing image to bucket called ${key}`);
+		console.log(`writing image to bucket called ${key}`);
 
-        await s3
-            .putObject({
-                Body: buffer,
-                Key: key,
-                ContentType: body.mime,
-                Bucket: process.env.imageUploadBucket,
-                ACL: 'public-read',
-            })
-            .promise();
+		await s3
+				.putObject({
+						Body: buffer,
+						Key: key,
+						ContentType: body.mime,
+						Bucket: process.env.imageUploadBucket,
+						ACL: 'public-read',
+				})
+				.promise();
 
-        const url = `https://${process.env.imageUploadBucket}.s3-${process.env.region}.amazonaws.com/${key}`;
-        return Response._200({
-            imageURL: url,
-        });
-    } catch (error) {
-        console.log('error', error);
+		const url = `https://${process.env.imageUploadBucket}.s3-${process.env.region}.amazonaws.com/${key}`;
+		return Response._200({
+				imageURL: url,
+		});
+	} catch (error) {
+		console.log('error', error);
 
-        return Response._400({ message: error.message || 'failed to upload image' });
-    }
+		return Response._400({ message: error.message || 'failed to upload image' });
+	}
 };
